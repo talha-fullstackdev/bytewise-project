1f6bcc8e65dd6afb8f75c5a7b41e0fde7a0a6397
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-10 12:33:55 -0800

    made header and styled it and implement linking to different pages

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Logo from './Logo'
-import Navbar from './Navbar'
+import React from "react";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+import { IoIosArrowRoundForward } from "react-icons/io";
 const Header = () => {
   return (
-    <header>
-        <Logo/>
-        <Navbar/>
-        <button>Register Now</button>
+    <header className="bg-gray-100 flex flex-col items-center gap-2 pt-4 md:flex-row md:justify-around md:p-10 ">
+      <Logo />
+      <Navbar />
+      <button className="bg-green-500 flex items-center gap-2 px-4 py-2 text-sm md:text-[14px] md:px-6 whitespace-nowrap rounded-md text-white hover:bg-green-400">
+        Register Now
+        <IoIosArrowRoundForward className="text-lg" />
+      </button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router'
 const Navbar = () => {
   return (
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contact</li>
-        <li>Pricing</li>
+    <ul className='flex flex-col md:flex-row md:gap-10 text-gray-500 font-semibold text-xl'>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/price">Pricing</Link>
     </ul>
   )
 }
