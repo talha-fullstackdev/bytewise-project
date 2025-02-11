a598e46a02e64871099d5350f7463549dde535ae
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-11 11:34:51 -0800

    made pixel grade section

---
+++ src/components/PixelGrade/PixelGrade.jsx
@@ -0,0 +1,53 @@
+// import React from 'react'
+// import Image from "../../assets/unlock-img.svg"
+// const PixelGrade = () => {
+//   return (
+//     <div className='flex flex-col mt-4 items-center px-4 md:flex-row md:gap-10'>
+//         <div className="md:ml-[90px]">
+//         <img className='md:w-[600px]' src={Image} alt="image" />
+//         </div>
+//         <div className="flex flex-col gap-3 md:w-[500px] md:mr-16">
+//             <p className='text-[#4D4D4D] text-md md:text-4xl font-bold md:font-semibold md:max-w-[500px]'>The unseen of spending three years at Pixelgrade</p>
+//             <p className='text-[#4D4D4D] text-sm  md:text-1xl  '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+//             <button className='bg-[#45d449] hover:bg-[#4be751] text-white w-[100px] items-center m-auto py-2 rounded-md '>Learn More</button>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default PixelGrade
+import React from "react";
+import Image from "../../assets/unlock-img.svg";
+
+const PixelGrade = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center px-6 md:px-16 lg:px-24 gap-6 md:gap-12 mt-8">
+      {/* Image Section */}
+      <div className="w-full md:w-1/2 flex justify-center">
+        <img
+          className="w-full max-w-[500px] md:max-w-[600px] object-contain"
+          src={Image}
+          alt="Unlocking Creativity"
+        />
+      </div>
+
+      {/* Text Content Section */}
+      <div className="w-full md:w-1/2 flex flex-col gap-4">
+        <p className="text-[#4D4D4D] text-lg md:text-3xl lg:text-4xl font-semibold leading-tight">
+          The unseen of spending three years at Pixelgrade
+        </p>
+        <p className="text-[#4D4D4D] text-sm md:text-base leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis.
+        </p>
+        {/* Button */}
+        <div className="flex justify-start md:justify-normal">
+          <button className="bg-[#45d449] m-auto hover:bg-[#3cbf3f] text-white px-5 py-2 rounded-lg transition duration-300 shadow-md">
+            Learn More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PixelGrade;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/hero/Hero'
 import OurClients from '../components/ourclients/OurClients'
 import Membership from '../components/membership/Membership'
+import PixelGrade from '../components/PixelGrade/PixelGrade'
 const HomePage = () => {
   return (
     <div className=''>
       <Hero/>
       <OurClients/>
       <Membership/>
+      <PixelGrade/>
     </div>
   )
 }
