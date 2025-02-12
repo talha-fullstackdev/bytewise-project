44a107ddb1656b57d0112713b7d3dcf86a51438c
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-11 16:33:14 -0800

    added margin from top to marketing section

--- src/components/marketing/Marketing.jsx
+++ src/components/marketing/Marketing.jsx
@@ -17,7 +17,7 @@ import MarketingDiv from './MarketingDiv';
 
 const Marketing = () => {
   return (
-    <div className="px-4 md:px-10 lg:px-20">
+    <div className="px-4 md:px-10 lg:px-20 md:mt-14">
       <MarketingDetails />
       <MarketingDiv />
     </div>
