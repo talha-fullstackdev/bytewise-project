3422a5b7a8b747d805107252a40d4919ce0b6ebb
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-12 15:25:05 -0800

    fill the pricing component

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { FaSquareWebAwesome } from "react-icons/fa6";
 
 const PricingPage = () => {
+  const data =[
+    {
+     icon:<FaSquareWebAwesome />,
+     name:"Web development",
+     price:"$29",
+     about:"we have the exciting discounts for you"
+    },
+    {
+     icon:<FaSquareWebAwesome />,
+     name:"Android development",
+     price:"$39",
+     about:"we have the exciting discounts for you"
+    },
+    {
+     icon:<FaSquareWebAwesome />,
+     name:"Designing",
+     price:"$20",
+     about:"we have the exciting discounts for you"
+    },
+  ]
   return (
     <div>
-      
+      <p>Find the plan that's right for you</p>
+      <div className="">
+        {data.map((item)=>(
+          <>
+          <img src={item.icon} alt="" />
+          <p>{item.name}</p>
+          <p>{item.about}</p>
+          <p>{item.price}</p>
+          <button>BUY NOW</button>
+          </>
+        ))}
+      </div>
     </div>
   )
 }
