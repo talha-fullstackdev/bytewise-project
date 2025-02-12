8a7b716867840ad9f1bb2783fac1202f92b05ead
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-12 10:38:34 -0800

    made fringilla section and add some basic responsiveness

--- src/components/fringilla/Fringilla.jsx
+++ src/components/fringilla/Fringilla.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
+// import React from 'react'
+// import { IoIosArrowRoundForward } from "react-icons/io";
+
+// const Fringilla = () => {
+//   return (
+//     <div className='bg-gray-100'>
+//         <div className="p-20 text-center">
+//             <p className='text-4xl md:w-[400px] md:m-auto md:font-semibold text-[#4D4D4D]'>Pellentesque suscipit fringilla libero eu.</p>
+//             <div className="flex items-center justify-center mt-2 bg-[#45d449] hover:bg-[#4be751] w-[130px] md:m-auto md:mt-6 py-2 text-gray-800 font-semibold">
+//             <button className="text-xl py-2">Get a demo</button>
+//             <span><IoIosArrowRoundForward />
+//             </span>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Fringilla
+import React from 'react';
 import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Fringilla = () => {
   return (
-    <div className='bg-gray-100'>
-        <div className="p-20 text-center">
-            <p className='text-4xl md:w-[400px] md:m-auto md:font-semibold text-[#4D4D4D]'>Pellentesque suscipit fringilla libero eu.</p>
-            <div className="flex items-center justify-center mt-2 bg-[#45d449] hover:bg-[#4be751] w-[130px] md:m-auto md:mt-6 py-2 text-gray-800 font-semibold">
-            <button>Get a demo</button>
-            <span><IoIosArrowRoundForward />
-            </span>
-            </div>
+    <div className='bg-gray-100 px-4 py-10 md:py-20'>
+      <div className="text-center">
+        <p className='text-2xl sm:text-3xl md:text-4xl md:w-[400px] md:m-auto font-semibold text-[#4D4D4D]'>
+          Pellentesque suscipit fringilla libero eu.
+        </p>
+        <div className="flex items-center justify-center  mt-4 md:mt-6 bg-[#45d449] hover:bg-[#4be751] w-[200px] md:w-[200px] m-auto py-1 md:py-3 px-2 md:px-6 text-gray-800 font-semibold rounded-lg cursor-pointer">
+          <button className="text-lg sm:text-xl py-2 text-gray-100">Get a demo</button>
+          <span className="text-2xl mt-1 ml-2 text-gray-100 ">
+            <IoIosArrowRoundForward />
+          </span>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Fringilla
+export default Fringilla;
