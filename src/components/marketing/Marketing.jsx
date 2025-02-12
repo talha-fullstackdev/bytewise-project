b51477a57389c9a1adc284875324157aa61517fe
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-11 16:34:09 -0800

    remove unwanted code from marketing section

--- src/components/marketing/Marketing.jsx
+++ src/components/marketing/Marketing.jsx
@@ -1,16 +1,3 @@
-// import React from 'react'
-// import MarketingDetails from './MarketingDetails'
-// import MarketingDiv from './MarketingDiv'
-// const Marketing = () => {
-//   return (
-//     <div>
-//         <MarketingDetails/>
-//         <MarketingDiv/>
-//     </div>
-//   )
-// }
-
-// export default Marketing
 import React from 'react';
 import MarketingDetails from './MarketingDetails';
 import MarketingDiv from './MarketingDiv';
