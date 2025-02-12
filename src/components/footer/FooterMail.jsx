34552d7cbccdf0699d4a52342b07c6eb77f8728b
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-12 11:19:09 -0800

    filled the email component

--- src/components/footer/FooterMail.jsx
+++ src/components/footer/FooterMail.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { FaLocationArrow } from "react-icons/fa6";
+
 const FooterMail = () => {
   return (
     <div>
-        <p></p>
+        <p>Stay up to date</p>
         <div className="">
         <input type="Your email address" />
+        <span><FaLocationArrow />
+        </span>
         </div>
     </div>
   )
--- src/components/footer/FooterNavigation.jsx
+++ src/components/footer/FooterNavigation.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const FooterNavigation = () => {
     const data = [
         {
