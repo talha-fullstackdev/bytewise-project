e002e0ff1ace51f4ac94b6a7044677febc551602
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-12 12:02:18 -0800

    add responsivenss to footer for mobile screen

--- src/components/footer/FooterMail.jsx
+++ src/components/footer/FooterMail.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+
+import React from 'react';
 import { FaLocationArrow } from "react-icons/fa6";
 
 const FooterMail = () => {
   return (
-    <div>
-        <p>Stay up to date</p>
-        <div className="">
-        <input type="Your email address" />
-        <span><FaLocationArrow />
+    <div className=" flex flex-col gap-2">
+      <p className="text-white">Stay up to date</p>
+      {/* Container for input and icon */}
+      <div className="flex items-center bg-[#38474d] rounded-md p-2">
+        {/* Input with removed default styles */}
+        <input 
+          type="email"
+          placeholder="Your email address"
+          className="bg-transparent outline-none text-white flex-1 px-2 placeholder-gray-300"
+        />
+        {/* Icon inside the input div */}
+        <span className="text-white cursor-pointer">
+          <FaLocationArrow />
         </span>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterMail
+export default FooterMail;
